f294398ac8eaa5edc91b9ed9eb38d6cbce21a473
Kerstin Wukoschitz <kerstin.wukoschitz@gmx.net>
2021-01-20 22:50:05 +0100

    comment in the outcommented stuff again

--- resources/views/partials/alerts.blade.php
+++ resources/views/partials/alerts.blade.php
@@ -1,29 +1,29 @@
-<div class="wrapper">
+{{--<div class="wrapper">--}}
 
 
-@foreach(['error' => 'danger', 'warning' => 'warning', 'info' => 'info', 'status' => 'info', 'success' => 'success'] as $key => $cn)
+{{--@foreach(['error' => 'danger', 'warning' => 'warning', 'info' => 'info', 'status' => 'info', 'success' => 'success'] as $key => $cn)--}}
 
-	@if($msg = Session::get($key))
-		<div class="alert alert-{{ $cn }}">
-			{{ trans($msg) }}
-		</div>
-	@endif
+{{--	@if($msg = Session::get($key))--}}
+{{--		<div class="alert alert-{{ $cn }}">--}}
+{{--			{{ trans($msg) }}--}}
+{{--		</div>--}}
+{{--	@endif--}}
 
-@endforeach
+{{--@endforeach--}}
 
-@if(is_object($errors) && $errors->any())
-	@if($errors->count() > 1)
-		<div class="alert alert-block alert-danger">
-			<ul>
-				@foreach ($errors->all() as $error)
-					<li>{{ $error }}</li>
-				@endforeach
-			</ul>
-		</div>
-	@else
-		<div class="alert alert-danger">
-			{{ $errors->first() }}
-		</div>
-	@endif
-@endif
-</div>
+{{--@if(is_object($errors) && $errors->any())--}}
+{{--	@if($errors->count() > 1)--}}
+{{--		<div class="alert alert-block alert-danger">--}}
+{{--			<ul>--}}
+{{--				@foreach ($errors->all() as $error)--}}
+{{--					<li>{{ $error }}</li>--}}
+{{--				@endforeach--}}
+{{--			</ul>--}}
+{{--		</div>--}}
+{{--	@else--}}
+{{--		<div class="alert alert-danger">--}}
+{{--			{{ $errors->first() }}--}}
+{{--		</div>--}}
+{{--	@endif--}}
+{{--@endif--}}
+{{--</div>--}}
